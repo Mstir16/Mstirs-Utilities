3d4345fd0abff5453c74a53fe3a5f06e3f7306a0
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2022-07-07 23:12:04 -0400

    Update ScriptFetcher.lua

--- Roblox/ScriptFetcher.lua
+++ Roblox/ScriptFetcher.lua
@@ -1,3 +1,11 @@
-pcall(function()
-    loadstring(game:HttpGet(""))()
+local y,n = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mstir16/Mstirs-Utilities/main/Roblox/"..game.PlaceId..".lua"))()
 end)
+
+if not y then
+   game.StarterGui:SetCore("SendNotification", {
+        Title = "Mstir's Utilities";
+        Text = "No Script for this game!";
+        Duration = 10;
+    });
+end
