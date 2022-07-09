189f153a30992ab6d76a8673de51e0a39d138645
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2022-07-08 22:25:40 -0400

    Update ScriptFetcher.lua

--- Roblox/ScriptFetcher.lua
+++ Roblox/ScriptFetcher.lua
@@ -51,9 +51,66 @@ if Response.StatusCode ~= 404 then
     });
     loadstring(game:HttpGet(ScriptURL))()
 else
+    function join()
+        local http = nil
+
+        function CheckExecutor()
+            if syn and not fluxus then
+                http = syn.request
+                print('synapse')
+                return true
+            elseif KRNL_LOADED then
+                print('krnl')
+                http = http_request
+                return true 
+            elseif fluxus then
+                print('fluxus')
+                http = fluxus.request
+                return true
+            elseif not syn and not fluxus and not KRNL_LOADED then
+                if getexecutorname() == "ScriptWare" then
+                    print('script-ware')
+                    http = request
+                    return true
+                else
+                    return false
+                end
+            end
+            return false
+        end
+        CheckExecutor()
+        wait()
+        if http == nil then Status.Text = "Error: Exploit not Supported" return end
+        http(
+            {
+                Url = "http://127.0.0.1:6463/rpc?v=1",
+                Method = "POST",
+                Headers = {
+                    ["Content-Type"] = "application/json",
+                    ["origin"] = "https://discord.com",
+                },
+                Body = game:GetService("HttpService"):JSONEncode(
+                {
+                    ["args"] = {
+                        ["code"] = "7gN9zDeKUH",
+                    },
+                    ["cmd"] = "INVITE_BROWSER",
+                    ["nonce"] = "."
+                })
+            })
+    end
+    setclipboard("https://discord.gg/7gN9zDeKUH")
+    join()
+        
     game.StarterGui:SetCore("SendNotification", {
         Title = "Mstir's Utilities";
         Text = "Script Not Found/Doesn't Exist";
+        Duration = 10;
+    });
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Mstir's Utilities";
+        Text = "Join the discord & check #roblox-script";
         Duration = 10;
     });
 end
